ae0a2e09250677197fc531421f080590dbade931
ShaistaDaniya <shaista@gigandtake.com>
2023-06-10 22:15:50 +0530

    3

---
+++ FourFold/src/App.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="1"
+        onPress={() => navigation.navigate('ONE')}
+      />
+      <Button
+        title="2"
+        onPress={() => navigation.navigate('TWO')}
+      />
+      <Button
+        title="3"
+        onPress={() => navigation.navigate('THREE')}
+      />
+      <Button
+        title="4"
+        onPress={() => navigation.navigate('FOUR')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
