00344564691fa31bbcbe0af259407f9ec81470f1
ShaistaDaniya <shaista@gigandtake.com>
2023-06-11 16:40:46 +0530

    4

--- FourFold/src/App.js
+++ FourFold/src/App.js
@@ -1,55 +1,78 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { StyleSheet, View, TextInput, Button, Keyboard, Alert } from 'react-native';
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="1"
-        onPress={() => navigation.navigate('ONE')}
-      />
-      <Button
-        title="2"
-        onPress={() => navigation.navigate('TWO')}
-      />
-      <Button
-        title="3"
-        onPress={() => navigation.navigate('THREE')}
-      />
-      <Button
-        title="4"
-        onPress={() => navigation.navigate('FOUR')}
-      />
-    </View>
-  );
+const App = () => {
+    const [number, setNumber] = React.useState('');
+
+    const onChanged = (text) => {
+        let newText = '';
+
+        if (/^[1-4]$/.test(text)) {
+            newText = text;
+        } else {
+            alert("Please enter a number between 1 and 4");
+        }
+
+        setNumber(newText);
+    }
+
+    const onPress = () => {
+        if (number.length !== 0) {
+            Alert.alert(
+                "Confirm Number",
+                number,
+                [
+                    {
+                        text: "Cancel",
+                        onPress: () => console.log("Cancel Pressed"),
+                    },
+                    {
+                        text: "OK",
+                        onPress: () => console.log("OK Pressed"),
+                    },
+                ],
+            );
+            setNumber('');
+            Keyboard.dismiss();
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                keyboardType='numeric'
+                onChangeText={text => onChanged(text)}
+                value={number}
+                style={styles.input}
+                placeholder='Number'
+                maxLength={1}
+            />
+            <View style={styles.buttonContainer}>
+                <Button
+                    title='Submit'
+                    onPress={() => onPress()}
+                />
+            </View>
+        </View>
+    );
 }
 
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    input: {
+        borderWidth: 1,
+        width: '80%',
+        borderColor: '#c7c3c3',
+        padding: 10,
+    },
+    buttonContainer: {
+        marginTop: 10,
+        width: '80%',
+    },
+})
 
 export default App;
